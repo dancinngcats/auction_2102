0d207b3062cac6d41f64b2bc9d2828935850c263
Kseniya K <kseniya.kudelich@gmail.com>
2021-03-02 13:10:17 -0700

    Add Attendee and Attendee test

---
+++ test/attendee_test.rb
@@ -0,0 +1,17 @@
+require_relative 'test_helper'
+
+class AttendeeTest < Minitest::Test
+
+  def setup
+    @attendee = Attendee.new(name: 'Megan', budget: '$50')
+  end
+
+  def test_it_exists
+    assert_instance_of Attendee, @attendee
+  end
+
+  def test_it_has_attributes
+    assert_equal "Megan", @attendee.name
+    assert_equal 50, @attendee.budget
+  end
+end
